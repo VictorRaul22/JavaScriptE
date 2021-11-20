a7dcd34c368220bf6fba76a4ea0bfe271be478b2
Victor <victor-raul-22@hotmail.com>
2021-11-19 20:15:30 -0500

    Comienzo

---
+++ js/index_dom.js
@@ -0,0 +1,8 @@
+import hamburgerMenu from "./menu_hamburguesa.js";
+import { reloj } from "./reloj_alarma.js";
+const d=document;
+d.addEventListener("DOMContentLoaded",e=>{
+    hamburgerMenu(".panel-btn",".panel",".menu a");
+    reloj("#iniciarR","#detenerR","#reloj h2","#reloj");
+})
+
