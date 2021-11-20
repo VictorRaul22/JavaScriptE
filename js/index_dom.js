fe4f5fd29b68a45f3b4218eae038e4c65cf7519a
Victor <victor-raul-22@hotmail.com>
2021-11-19 22:53:06 -0500

    Reloj y Alarma

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
-import { reloj } from "./reloj_alarma.js";
+ import { digitalClock,alarm } from "./reloj.js";
 const d=document;
 d.addEventListener("DOMContentLoaded",e=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
+    alarm("assets/Alarma.mp3","#activar-alarma","#desactivar-alarma");
 })
 
