fd1a05e9955671e57af73e1ac40c747c9b863ce4
Victor <victor-raul-22@hotmail.com>
2021-12-15 19:03:50 -0500

    sorteo

--- js/sorteo.js
+++ js/sorteo.js
@@ -1,12 +1,16 @@
 const d=document;
-export default function sorteo(items,btn){
-    const $items=d.querySelectorAll(items),
-        $btn=d.querySelector(btn);
-    console.log($items)
-    d.addEventListener("click",(e)=>{
+export default function draw(btn,selector){
+    const getWinner=(selector)=>{
+        const $players=d.querySelectorAll(selector),
+        random=Math.floor(Math.random()*$players.length),
+        winner=$players[random];
+        return `El ganador es ${winner.textContent}`
+    }
+    d.addEventListener("click",e=>{
         if(e.target.matches(btn)){
-            let numero=Math.floor(Math.random() * (($items.length-1) - 0)) + 0;
-                alert($items[numero].textContent)
+            let result=getWinner(selector);
+            alert(result);
+            console.log(result)
         }
     })
 }
